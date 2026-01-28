173067770cc516012ee5321f20425b1c4204dc95
Alex3455755 <alexeigyll@gmail.com>
2026-01-28 13:33:38 +0500

    Доработки

--- frontend/testtask/src/components/EmploeeList.vue
+++ frontend/testtask/src/components/EmploeeList.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <!-- ОСНОВНАЯ ТАБЛИЦА СОТРУДНИКОВ -->
     <table border="1" style="border-collapse: collapse; width: 100%;">
       <thead>
         <tr>
@@ -12,20 +13,25 @@
         </tr>
       </thead>
       <tbody>
+        <!-- ЦИКЛ ПО СОТРУДНИКАМ - v-for создает строку для каждого сотрудника -->
         <tr v-for="emp in employees" :key="emp.firstName">
           <td>{{ emp.firstName }}</td>
           <td>{{ emp.lastName }}</td>
           <td>{{ emp.age }}</td>
           <td>{{ emp.experience }} года</td>
           <td>{{ emp.address }}</td>
+          <!-- КНОПКА РЕДАКТИРОВАНИЯ - передает объект сотрудника в метод -->
           <td>
             <button @click="openEditModal(emp)">Редактировать</button>
           </td>
         </tr>
       </tbody>
     </table>
+    
+    <!-- КНОПКА ДОБАВЛЕНИЯ - открывает модалку для нового сотрудника -->
     <button id="btnAdd" @click="addEmploee">Добавить сотрудника</button>
     
+    <!-- МОДАЛЬНОЕ ОКНО - показывается условно через v-if -->
     <ModalEmploee
       v-if="showEditModal" 
       :employee="selectedEmployee"
@@ -44,9 +50,10 @@ export default {
   },
   data() {
     return {
+      // МАССИВ СОТРУДНИКОВ - основное хранилище данных
       employees: [
         {
-            id: 1,
+          id: 1,
           firstName: 'Ваня',
           lastName: 'Иванов',
           age: 20,
@@ -54,7 +61,7 @@ export default {
           address: 'Ленина 15',
         },
         {
-            id: 2,
+          id: 2,
           firstName: 'Аня',
           lastName: 'Сидорова',
           age: 22,
@@ -62,35 +69,54 @@ export default {
           address: 'Старцева 7',
         }
       ],
-      showEditModal: false,
-      selectedEmployee: null
+      // ВЫБРАННЫЙ СОТРУДНИК - для редактирования/создания
+      selectedEmployee: null,
+      // ВИДИМОСТЬ МОДАЛКИ - true/false для показа/скрытия
+      showEditModal: false
     };
   },
   methods: {
-    addEmploee(){
-        this.selectedEmployee = {};
-        this.showEditModal = true;
+    // ДОБАВЛЕНИЕ НОВОГО - создает пустой объект для новой записи
+    addEmploee() {
+      this.selectedEmployee = {}; // ПУСТОЙ ОБЪЕКТ для новой записи
+      this.showEditModal = true;
     },
+    
+    // РЕДАКТИРОВАНИЕ - создает КОПИЮ объекта для безопасного редактирования
     openEditModal(employee) {
+      // {...employee} - поверхностное копирование (shallow copy)
+      // Зачем? Изменения в модалке не затронут оригинал до сохранения
       this.selectedEmployee = { ...employee };
       this.showEditModal = true;
     },
+    
+    // ЗАКРЫТИЕ МОДАЛКИ - сбрасывает состояние
     closeEditModal() {
       this.showEditModal = false;
       this.selectedEmployee = null;
     },
+    
+    // САМЫЙ СЛОЖНЫЙ МЕТОД - сохранение изменений
     handleEmployeeUpdate(updatedEmployee) {
+      // НАХОДИМ ПО ID - findIndex возвращает индекс или -1
       const index = this.employees.findIndex(emp => 
-        emp.id === updatedEmployee.id
+        emp.id === updatedEmployee.id  // СРАВНИВАЕМ ID
       );
+      
+      // РЕДАКТИРОВАНИЕ СУЩЕСТВУЮЩЕГО
       if (index !== -1) {
-        
         this.employees[index] = updatedEmployee;
-
-      }else{
+      } 
+      // ДОБАВЛЕНИЕ НОВОГО (нет ID или не найден)
+      else {
+        // ВНИМАНИЕ: для новых сотрудников нужно добавить ID!
+        // Можно использовать Date.now() или счетчик
+        updatedEmployee.id = Date.now();
+        console.log(updatedEmployee.id);
         this.employees.push(updatedEmployee);
       }
-      this.closeEditModal();
+      
+      this.closeEditModal(); // ОЧИСТКА после сохранения
     }
   }
 }
